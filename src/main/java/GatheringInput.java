a7e8ea521b12b913ebcdd81e1697ad67bc2be4e4
nevioxis <pokkrong.p.p@gmail.com>
2020-04-12 02:22:08 +0700

    +  Easily File Operation (Readlines/Writelines) : FileP +  Gathering Data from input.txt : GatheringInput

---
+++ src/main/java/GatheringInput.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+public class GatheringInput
+{
+    ArrayList<String> input;
+    ArrayList<AccountID> accounts;
+    ArrayList<BusID> Buses;
+    ArrayList<EventID> events;
+}
+
+
+
+
